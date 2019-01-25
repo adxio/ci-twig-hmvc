40db04e88c5247f206d9835f496a8e6e2101bb38
Andi <nakamichikun@gmail.com>
2019-01-26 01:24:54 +0700

    Installation

---
+++ install.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Part of CodeIgniter Twig & HMVC
+ *
+ * @author     Andi Kurniawan <https://github.com/nakamichikun>
+ * @license    MIT License
+ * @copyright  2019 Andi kurniawan
+ * @link       https://github.com/nakamichikun/ci-twig-hmvc
+ */
+
+$installer = new Installer();
+$installer->install();
+
+class Installer
+{
+	public static function install()
+	{
+		self::recursiveCopy('vendor/codeigniter/framework', '.');
+		self::copy('Twig.php', 'application/libraries/Twig.php');
+	}
+
+	private static function copy($src, $dst)
+	{
+		$success = copy($src, $dst);
+		if ($success) {
+			echo 'copied: ' . $dst . PHP_EOL;
+		}
+	}
+
+	/**
+	 * Recursive Copy
+	 *
+	 * @param string $src
+	 * @param string $dst
+	 */
+	private static function recursiveCopy($src, $dst)
+	{
+		@mkdir($dst, 0755);
+		
+		$iterator = new \RecursiveIteratorIterator(
+			new \RecursiveDirectoryIterator($src, \RecursiveDirectoryIterator::SKIP_DOTS),
+			\RecursiveIteratorIterator::SELF_FIRST
+		);
+		
+		foreach ($iterator as $file) {
+			if ($file->isDir()) {
+				@mkdir($dst . '/' . $iterator->getSubPathName());
+			} else {
+				if($iterator->getSubPathName() != 'composer.json') {
+					$success = copy($file, $dst . '/' . $iterator->getSubPathName());
+					if ($success) {
+						echo 'copied: ' . $dst . '/' . $iterator->getSubPathName() . PHP_EOL;
+					}
+				}
+			}
+		}
+	}
+}
